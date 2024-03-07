e0e9952d6372ce8b2808a2efe2cc6be235e7ef61
putafilehere <matthew.fitch26@d155.org>
2024-03-07 08:10:39 -0600

    removed garbage, dont need collision or rotation really, doing chess now

--- src/Phase1.java
+++ src/Phase1.java
@@ -15,62 +15,21 @@ public class Phase1 extends JPanel {
 
     private Sprite bob;
 
-    private double funny = 0;
-
     // initialization of everything
     public Phase1(int width, int height) {
         this.width = width;
         this.height = height;
         setBackground(new Color(60, 50, 65));
         player = new Sprite(new Vec2(0, 0), "images/hands/fist.png", new Vec2(100, 100), 45);
-        objs.add(player);
-        bob = new Sprite(new Vec2(100, 100), "images/hands/peace.png", new Vec2(200, 200), 130);
-        objs.add(bob);
         addKeyListener(new MyKeyListener());
         setFocusable(true); // Ensure the panel can receive key events
-
     }
 
     @Override
     public void paintComponent(Graphics g) {
-        funny += 0.01;
         super.paintComponent(g);
         for (GameObject thing : objs) {
-            if (pressedKeys.contains('w')) {
-                if (player.getVel().getY() == 0)
-                    player.addVel(new Vec2(0, -5));
-            } else if (player.getVel().getY() == -5) {
-                player.addVel(new Vec2(0, 5));
-            }
-            if (pressedKeys.contains('a')) {
-                if (player.getVel().getX() == 0)
-                    player.addVel(new Vec2(-5, 0));
-            } else if (player.getVel().getX() == -5) {
-                player.addVel(new Vec2(5, 0));
-            }
-            if (pressedKeys.contains('s')) {
-                if (player.getVel().getY() == 0)
-                    player.setVel(new Vec2(0, 5));
-            } else if (player.getVel().getY() == 5) {
-                player.addVel(new Vec2(0, -5));
-            }
-            if (pressedKeys.contains('d')) {
-                if (player.getVel().getX() == 0)
-                    player.addVel(new Vec2(5, 0));
-            } else if (player.getVel().getX() == 5) {
-                player.addVel(new Vec2(-5, 0));
-            }
-            int yPos = thing.getPos().getY();
-            int yVel = thing.getVel().getY();
-            int xPos = thing.getPos().getX();
-            int xVel = thing.getVel().getX();
-            thing.setPos(new Vec2(xPos + xVel, yPos + yVel));
             thing.draw(g);
-            for (GameObject other : objs)
-                if (other != thing)
-                    if (thing.isColliding(other))
-                        System.out.println("AHHH IM HURT");
-            thing.setAngle((thing.getAngle()+funny)%360);
         }
         try {
             Thread.sleep(10);
